c79db630476facc8a980f5ebea8b58d79d01f6c3
Barbara <barbaradibattista@yahoo.fr>
2016-09-29 12:14:17 +0200

    ~img home css, ~js scroll

--- js/index.js
+++ js/index.js
@@ -1,35 +1,12 @@
 
 /* animation.js/scrollTop */
+$( document ).ready(function() {
+  $('a[href^="#"]').click(function(){
+	var id = $(this).attr("href");
+  var offset = $(id).offset().top 
+$('html, body').animate({scrollTop: offset}, 'slow'); 
+    return false;
+  }); 
 
-$('a[href^="#"]').click(function(){
-	var jean-michel = $(this).attr("href");
-
-	$('html, body').animate({
-		scrollTop:$('#jean-michel').offset().top
-	}, 5000);
-	return false;
 });
-
-
-
-
-
-/*$(document).ready(function() {
-   
-    $('.yolo').click(function(){
-        $('html, body').animate({scrollTop:$(document).height()}, 500);
-        return false;
-    });
-
-});*/
-
-
-/*$(document).ready(function(){
-   $('.yolo').click(function(){
-      $('html, body').animate({
-       scrollTop: $($(this).attr('href','#jean-michel')).offset().top
-      }, 2000);
-       
-   });
-});*/
 
